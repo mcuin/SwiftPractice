8d2f6884a42516f85e7ace354f949a659957e165
Mykal Cuin <mykcuin@gmail.com>
2014-09-26 11:47:09 -0600

    Finished control flow.

--- langTour/tour.swift
+++ langTour/tour.swift
@@ -71,4 +71,43 @@ switch vegetable {
 		let vegetableComment = "Everything tastes good in soup."
 }
 
-	
+let interstingNumber = ["Prime": [2, 3, 5, 7, 11, 13],
+			"Fibbonacci": [1, 1, 2, 3, 5, 8],
+			"Square": [1, 4, 9, 16, 25],
+]
+
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+	for numbers in numbers {
+		if number > largest {
+			largest = number
+		}
+	}
+}
+largest
+
+var n = 2
+whine n < 100 {
+	n = n * 2
+}
+n
+
+var m = 2
+do {
+	m = m *2
+} while m < 100
+m
+
+var firstForLoop = 0
+for i in 0.. < 4 {
+	firstForLoop += i
+}
+firstForLoop
+
+var secondForLoop = 0
+for var i = 0; i < 4; ++i {
+	secondForLoop += i
+}
+secondForLoop
+
+ 	
