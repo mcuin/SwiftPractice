78d2355836ebcb8a5d9c595991ce4aca0d90ced4
Mykal Cuin <mykcuin@gmail.com>
2014-09-26 10:52:17 -0600

    Working with control flows.

--- langTour/tour.swift
+++ langTour/tour.swift
@@ -36,3 +36,39 @@ occupations["Jayne"] = "Public Relations"
 
 let emptyArray = (String)[]
 let emptyDictionary = (String: Float)[]
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+	if score > 50 {
+		teamScore += 3
+	} else {
+		teamScore += 1
+	}
+}
+teamScore
+
+var optionalString: String? = "Hello"
+optionalString == nil
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+	greeting = "Hello, \(name)"
+} else if let name = nil {
+	greeting = "Hello nobody."
+}
+
+let vegetable = "red pepper"
+switch vegetable { 
+	case "celery":
+		let vegetableComment = "Add some raisins and make ants on a log."
+	case "cucumber", "watercress":
+		let vegetableComment = "That would make a good tea sandwich."
+	case let x where x.hasSuffix("pepper"):
+		let vegetableComment = "Is it spicy \(x)?"
+	default:
+		let vegetableComment = "Everything tastes good in soup."
+}
+
+	
