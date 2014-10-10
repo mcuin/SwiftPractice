b2a26a0d4222d58c2f235350b3665dd546b2d55b
Mykal Cuin <mykcuin@gmail.com>
2014-10-10 09:41:53 -0600

    Finished with protocols.

--- langTour/tour.swift
+++ langTour/tour.swift
@@ -434,4 +434,46 @@ switch success {
 		let serverResponse = "Sunrise at \(sunrise) and sunset at \(sunset)."
 	case let .Error(error):
 		let serverResponse = "Failure... \(error)";
+}
+
+protocol ExampleProtocol {
+	var simpleDescription: String {get}
+	mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+	var simpleDescription: String = "A very simple class."
+	var anotherProperty: Int = 69105
+	func adjust() {
+		simpleDescription += " Now 100% adjusted."
+	}
+}
+
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+
+struct simpleStructure: ExampleProtocol {
+	var simpleDescription: String = "A simple structure."
+	mutating func adjust() {
+		simpleDescription += " (adjusted)"
+	}
+}
+
+var b = simpleStrucutre()
+b.adjust()
+let bDescription = b.simpleDescription
+
+extension Int: ExampleProtocol {
+	var simpleDescription: String {
+		return "The number \(self)"
+	}
+	mutating func adjust() {
+		self += 42
+	}
+
+simpleDescription
+
+
+	
 
