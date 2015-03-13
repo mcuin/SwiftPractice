708c0643e61f197361572d0bc2a89c334ca8481c
Mykal Cuin <mykcuin@gmail.com>
2015-03-13 12:00:03 -0600

    Speeding up the table.

--- Developing iOS8/HelloWorld/HelloWorld/SearchResultsViewController.swift
+++ Developing iOS8/HelloWorld/HelloWorld/SearchResultsViewController.swift
@@ -14,6 +14,7 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
     var tableData = []
     var api: APIController = APIController()
     let kCellIdentifer: String = "SearchResultCell"
+    var imageCache = [String : UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,13 +36,39 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifer) as UITableViewCell
         
-        let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
         cell.textLabel?.text = rowData["trackName"] as? String
+        cell.imageView?.image = UIImage(named: "Blank52")
         let urlString: NSString = rowData["artworkUrl60"] as NSString
-        let imageUrl: NSURL? = NSURL(string: urlString)
-        let imgData = NSData(contentsOfURL: imageUrl!)
-        cell.imageView?.image = UIImage(data: imgData!)
         let formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        var image = self.imageCache[urlString]
+        
+        if (image == nil) {
+            var imageUrl: NSURL? = NSURL(string: urlString)
+            let request: NSURLRequest = NSURLRequest(URL: imageUrl!)
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+                if error == nil {
+                    image = UIImage(data: data)
+                    
+                    self.imageCache[urlString] = image
+                    dispatch_async(dispatch_get_main_queue(), {
+                        if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath) {
+                            cellToUpdate.imageView?.image = image
+                        }
+                    })
+                } else {
+                    println("Error \(error.localizedDescription)")
+                }
+            })
+                
+        } else {
+            dispatch_async(dispatch_get_main_queue(), {
+                if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath) {
+                    cellToUpdate.imageView?.image = image
+                }
+            })
+        }
+        
         cell.detailTextLabel?.text = formattedPrice
         
         return cell
