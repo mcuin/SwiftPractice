58695cbdaeac4b3acb20e670325c64acf488f46c
Mykal Cuin <mykalcuin@Mykals-MacBook-Pro.local>
2015-03-13 09:33:44 -0600

    Making the API request.

--- Developing iOS8/HelloWorld/HelloWorld/ViewController.swift
+++ Developing iOS8/HelloWorld/HelloWorld/ViewController.swift
@@ -35,6 +35,38 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
+    
+    func searchItunesFor(searchTerm: String) {
+        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        if let escapedSearchTerm = itunesSearchTerm.stringByReplacingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let url = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in println("Task Completed")
+                
+                if (error != nil) {
+                    println(error.localizedDescription)
+                }
+                
+                var err: NSError?
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                
+                if (err != nil) {
+                    println("JSON Error: \(err!.localizedDescription)")
+                }
+                
+                let result: NSArray = jsonResult["results"] as NSArray
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableData = result
+                    self.appsTableView!.reloadData()
+                    })
+            })
+            
+            task.resume()
+        }
+    }
 
 
 }
